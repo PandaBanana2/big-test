00ee9a12542ba8986fefcdf887ea5304bf5d5a06
rio <rio@pandaBanana.localdomain>
2025-11-21 18:39:16 -0500

    test

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import express from 'express';
 const app = express();
 
+// Serve index.html for the root path
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 //app.get('/', (req, res) => {
 //  res.send('Hello World!')
 //});
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve index.html for the root path
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 export default app;
