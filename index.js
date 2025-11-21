736ee5d56e1f86c42d031acacaad2c1e8a449f64
rio <rio@pandaBanana.localdomain>
2025-11-21 18:32:52 -0500

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 
 // Serve index.html for the root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile('public/index.html');
 });
 
 export default app;
