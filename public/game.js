9ad096c5c82645e278fbf2554da4370ab6a916f3
rio <rio@pandaBanana.localdomain>
2025-11-21 19:38:10 -0500

    retry

--- public/game.js
+++ public/game.js
@@ -1,5 +1,7 @@
 document.addEventListener('keyup', (event) => {
   if(event.key == 'ArrowUp'){
     console.log('up arrow pressed')
+    const rect = document.getElementById('rect');
+    rect.style.top = parseInt(rect.style.top,10) + 10 + "px"
   }
 })
