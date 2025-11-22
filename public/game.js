8fe30686fbeae2ddd344e249ff3369fe3c025dac
rio <rio@pandaBanana.localdomain>
2025-11-21 20:17:07 -0500

    fixed bugs

--- public/game.js
+++ public/game.js
@@ -1,7 +1,7 @@
 document.addEventListener('keyup', (event) => {
   if(event.key == 'ArrowUp'){
     const rect = document.getElementById('rectangle');
-    rect.style.top = parseInt(window.getComputedStyle('rect').getPropertyValue('top'),10) + 10 + "px";
+    rect.style.top = parseInt(window.getComputedStyle(rect).getPropertyValue('top'),10) + 10 + "px";
     console.log('hi',rect.style.top);
   }
 })
